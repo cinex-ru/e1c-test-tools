71d6eb420f81a1862dcda47cfca4c0850c88b117
cinex-ru <74475837+cinex-ru@users.noreply.github.com>
2020-12-26 07:04:30 +0700

    style: fix tests description

--- tests/tester.spec.ts
+++ tests/tester.spec.ts
@@ -4,7 +4,7 @@ import TesterEpf, { Форма } from './__e1c-mocks__/TesterEpf';
 const testerEpfMock = new TesterEpf();
 
 describe('Когда производится вычисление', () => {
-    it('должно корректно вычислять выражение', async () => {
+    it('должен корректно вычислять выражение', async () => {
         const expression = '6*7';
         const result = await performTask(createEvaluateTaskRequest({ expression }));
 
@@ -13,7 +13,7 @@ describe('Когда производится вычисление', () => {
 });
 
 describe('Когда выполняется код', () => {
-    it('должно возвращать результат выполнения', async () => {
+    it('должен возвращать результат выполнения', async () => {
         const code = 'Результат = 6*7;';
         const result = await performTask(createExecuteTaskRequest({ code }));
 
@@ -22,14 +22,14 @@ describe('Когда выполняется код', () => {
 });
 
 describe('Когда производится выполнение кода доступного только на сервере', () => {
-    it('должно возвращать корректный результат на сервере', async () => {
+    it('должен возвращать корректный результат на сервере', async () => {
         const code = 'Запрос = Новый Запрос("123"); Результат = Запрос.Текст;';
         const result = await performTask(createExecuteTaskRequest({ code }, true));
 
         expect(result).toBe('123');
     });
     // eslint-disable-next-line jest/no-disabled-tests
-    it.skip('должно вызывать исключение на клиенте', async () => {
+    it.skip('должен вызывать исключение на клиенте', async () => {
         const code = 'Запрос = Новый Запрос("123"); Результат = Запрос.Текст;';
         const result = await performTask(createExecuteTaskRequest({ code }));
 
@@ -38,14 +38,14 @@ describe('Когда производится выполнение кода до
 });
 
 describe('Когда производится вызов метода доступного только на клиенте', () => {
-    it('должно возвращать корректный результат на клиенте', async () => {
+    it('должен возвращать корректный результат на клиенте', async () => {
         const expression = 'ПолучитьОкна().Количество()';
         const result = await performTask(createEvaluateTaskRequest({ expression }));
 
         expect(typeof result).toBe('number');
     });
     // eslint-disable-next-line jest/no-disabled-tests
-    it.skip('должно вызывать исключение на сервере', async () => {
+    it.skip('должен вызывать исключение на сервере', async () => {
         const expression = 'ПолучитьОкна().Количество()';
         const result = await performTask(createEvaluateTaskRequest({ expression }, true));
 
@@ -54,7 +54,7 @@ describe('Когда производится вызов метода досту
 });
 
 describe('Когда производится вызов метода формы "Форма"', () => {
-    describe('и имя метода "ПолучитьТелоОтвета"', () => {
+    describe('и имя метода "ПодготовитьТелоОтвета"', () => {
         it('должен быть получен объект из входных параметров', async () => {
             const форма = testerEpfMock.ПолучитьФорму(Форма);
 
